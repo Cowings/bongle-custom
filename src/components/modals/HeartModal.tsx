c2250c705bdcdc12f3b1ced6118b89d704606baf
Cow <ILoveCowz@protonmail.com>
2022-04-10 18:05:03 -0700

    Update HeartModal.tsx

--- src/components/modals/HeartModal.tsx
+++ src/components/modals/HeartModal.tsx
@@ -9,7 +9,7 @@ export const HeartModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="Thank You" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
-        Bongle Infinite was created by Theodore Bong because just 1 word per day didn't suffice. 
+        Bongle Custom was created by Theodore Bong to challenge friends with custom words. 
         Credits to the Original Wordle & React-Wordle for inspiration and guidance.
         All code is under a MIT license and can be found at my github: {' '}
         <a
